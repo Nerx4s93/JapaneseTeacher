10cc33c32a016b340d7cd1753af2a60f9343f887
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-25 19:06:50 +0300

    Изменение реализации сцен

--- GUI/Presenters/SceneForm.cs
+++ GUI/Presenters/SceneForm.cs
@@ -1,53 +1,97 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace JapaneseTeacher.GUI.Presenters
 {
     public partial class SceneForm : Form
     {
-        private int _sceneId;
-        private Dictionary<int, List<Control>> _controlsOnScene;
+        private int _currentSceneId = 0;
+        private readonly Dictionary<int, List<Control>> _sceneControls = new Dictionary<int, List<Control>>();
+        private bool _isInitialized = false;
 
-        public int SceneId
+        public int CurrentSceneId
         {
-            get
-            {
-                return _sceneId;
-            }
+            get => _currentSceneId;
             set
             {
-                if (_sceneId == value)
+                if (_currentSceneId == value || !_isInitialized)
                 {
                     return;
                 }
-
-                if (!_controlsOnScene.ContainsKey(_sceneId))
-                {
-                    _controlsOnScene[_sceneId] = new List<Control>();
-                }
-
-                foreach (Control control in Controls)
-                {
-                    _controlsOnScene[_sceneId].Add(control);
-                }
-
-                Controls.Clear();
-                if (_controlsOnScene.TryGetValue(value, out var controls))
-                {
-                    foreach (var control in controls)
-                    {
-                        Controls.Add(control);
-                    }
-                }
-
-                _sceneId = value;
+                SwitchScene(value);
             }
         }
 
-        public SceneForm() : base()
+        protected override void OnLoad(EventArgs e)
         {
-            _sceneId = 0;
-            _controlsOnScene = new Dictionary<int, List<Control>>();
+            base.OnLoad(e);
+            InitializeScenes();
+            _isInitialized = true;
+            ApplyScene(_currentSceneId);
+        }
+
+        private void SwitchScene(int newSceneId)
+        {
+            // Скрываем элементы текущей сцены
+            if (_sceneControls.TryGetValue(_currentSceneId, out var currentControls))
+            {
+                foreach (var control in currentControls)
+                {
+                    control.Visible = false;
+                }
+            }
+
+            // Показываем элементы новой сцены
+            ApplyScene(newSceneId);
+
+            _currentSceneId = newSceneId;
+        }
+
+        private void ApplyScene(int sceneId)
+        {
+            if (_sceneControls.TryGetValue(sceneId, out var sceneControls))
+            {
+                foreach (var control in sceneControls)
+                {
+                    control.Visible = true;
+                }
+            }
+        }
+
+        private void InitializeScenes()
+        {
+            var controls = GetAllControls(this);
+
+            foreach (var control in controls)
+            {
+                if (control.Tag != null && int.TryParse(control.Tag.ToString(), out int sceneId))
+                {
+                    if (!_sceneControls.ContainsKey(sceneId))
+                    {
+                        _sceneControls[sceneId] = new List<Control>();
+                    }
+                    _sceneControls[sceneId].Add(control);
+                    control.Visible = (sceneId == _currentSceneId);
+                }
+                else
+                {
+                    control.Visible = true;
+                }
+            }
+        }
+
+        private IEnumerable<Control> GetAllControls(Control control)
+        {
+            var controls = new List<Control>();
+
+            foreach (Control child in control.Controls)
+            {
+                controls.Add(child);
+                controls.AddRange(GetAllControls(child));
+            }
+
+            return controls;
         }
     }
 }
