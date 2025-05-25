1fbc7ab67f11dd33a8aaeab94411b6dedd2d54c3
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-25 18:32:41 +0300

    Реализация сцен

--- GUI/Presenters/SceneForm.cs
+++ GUI/Presenters/SceneForm.cs
@@ -1,10 +1,12 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace JapaneseTeacher.GUI.Presenters
 {
     public partial class SceneForm : Form
     {
         private int _sceneId;
+        private Dictionary<int, List<Control>> _controlsOnScene;
 
         public int SceneId
         {
@@ -14,13 +16,38 @@ namespace JapaneseTeacher.GUI.Presenters
             }
             set
             {
+                if (_sceneId == value)
+                {
+                    return;
+                }
+
+                if (!_controlsOnScene.ContainsKey(_sceneId))
+                {
+                    _controlsOnScene[_sceneId] = new List<Control>();
+                }
+
+                foreach (Control control in Controls)
+                {
+                    _controlsOnScene[_sceneId].Add(control);
+                }
+
+                Controls.Clear();
+                if (_controlsOnScene.TryGetValue(value, out var controls))
+                {
+                    foreach (var control in controls)
+                    {
+                        Controls.Add(control);
+                    }
+                }
+
                 _sceneId = value;
             }
         }
 
-        public SceneForm()
+        public SceneForm() : base()
         {
-            InitializeComponent();
+            _sceneId = 0;
+            _controlsOnScene = new Dictionary<int, List<Control>>();
         }
     }
 }
