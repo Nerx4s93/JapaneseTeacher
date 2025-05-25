856c496b742fb1e94c4f90d783edb3efdede6490
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-25 18:18:11 +0300

    Класс базовой формы, реализующей сцены

--- GUI/FormMain.cs
+++ GUI/FormMain.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using JapaneseTeacher.GUI.Presenters;
 
 namespace JapaneseTeacher.GUI
 {
-    public partial class FormMain : Form
+    public partial class FormMain : SceneForm
     {
         public FormMain()
         {
---
+++ GUI/Presenters/SceneForm.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace JapaneseTeacher.GUI.Presenters
+{
+    public partial class SceneForm : Form
+    {
+        private int _sceneId;
+
+        public int SceneId
+        {
+            get
+            {
+                return _sceneId;
+            }
+            set
+            {
+                _sceneId = value;
+            }
+        }
+
+        public SceneForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
