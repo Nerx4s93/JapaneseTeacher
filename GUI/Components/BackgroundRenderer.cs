73a895f1c7cd36f3655cb91addf3abe9c792c821
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-26 05:31:14 +0300

    Класс для отрисовки фона окна

---
+++ GUI/Components/BackgroundRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace JapaneseTeacher.GUI.Components
+{
+    internal class BackgroundRenderer
+    {
+        private readonly Form _form;
+        private readonly Image _image;
+
+        public BackgroundRenderer(Form form, Image image)
+        {
+            _form = form;
+            _image = image;
+        }
+    }
+}
