d52f1a9b84fd6325adeeaf8a5602eef50978df0d
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-26 05:43:29 +0300

    Включён параметр двойной буферизации в FormMain

--- GUI/FormMain.Designer.cs
+++ GUI/FormMain.Designer.cs
@@ -35,6 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DoubleBuffered = true;
             this.Name = "FormMain";
             this.Text = "FormMain";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
