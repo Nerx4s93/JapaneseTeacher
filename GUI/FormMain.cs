d1e8ca03ce5c957de54760a7d437850b0c011153
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-26 05:48:35 +0300

    Масштабирование фона

--- GUI/FormMain.cs
+++ GUI/FormMain.cs
@@ -1,4 +1,7 @@
-﻿using JapaneseTeacher.Data;
+﻿using System;
+using System.Drawing;
+
+using JapaneseTeacher.Data;
 using JapaneseTeacher.GUI.Components;
 using JapaneseTeacher.GUI.Presenters;
 using JapaneseTeacher.Properties;
@@ -13,7 +16,12 @@ namespace JapaneseTeacher.GUI
         {
             InitializeComponent();
             _globeData = globalData;
-            _backgroundRenderer = new BackgroundRenderer(this, Resources.MainFon);
+
+            var backgroundSize = Resources.MainFon.Size;
+            var newBackgroundSizeWidth = Convert.ToInt32(Convert.ToDouble(backgroundSize.Width) / 1.5);
+            var newBackgroundSizeHeight = Convert.ToInt32(Convert.ToDouble(backgroundSize.Height) / 1.5);
+            var backgroundImage = new Bitmap(Resources.MainFon, new Size(newBackgroundSizeWidth, newBackgroundSizeHeight));
+            _backgroundRenderer = new BackgroundRenderer(this, backgroundImage);
         }
     }
 }
