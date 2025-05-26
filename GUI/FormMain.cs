56e5a836a3da4b30660adf77e49573175e8cf61b
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-26 06:10:13 +0300

    Удаление масштабирования в FormMain

--- GUI/FormMain.cs
+++ GUI/FormMain.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Drawing;
-
-using JapaneseTeacher.Data;
+﻿using JapaneseTeacher.Data;
 using JapaneseTeacher.GUI.Components;
 using JapaneseTeacher.GUI.Presenters;
 using JapaneseTeacher.Properties;
@@ -17,11 +14,7 @@ namespace JapaneseTeacher.GUI
             InitializeComponent();
             _globeData = globalData;
 
-            var backgroundSize = Resources.MainFon.Size;
-            var newBackgroundSizeWidth = Convert.ToInt32(Convert.ToDouble(backgroundSize.Width) / 1.5);
-            var newBackgroundSizeHeight = Convert.ToInt32(Convert.ToDouble(backgroundSize.Height) / 1.5);
-            var backgroundImage = new Bitmap(Resources.MainFon, new Size(newBackgroundSizeWidth, newBackgroundSizeHeight));
-            _backgroundRenderer = new BackgroundRenderer(this, backgroundImage);
+            _backgroundRenderer = new BackgroundRenderer(this, Resources.MainFon);
         }
     }
 }
