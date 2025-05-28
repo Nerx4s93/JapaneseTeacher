9624d40341ed9e5faea61f6df94b24e6a5c414e1
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-28 18:47:07 +0300

    Дополнительные параметры

--- Lessons/Lesson.cs
+++ Lessons/Lesson.cs
@@ -2,13 +2,15 @@
 {
     internal class Lesson
     {
-        public readonly int CompliteLevels = 0;
         public readonly int Levels = 0;
+        public readonly int CompliteLevels = 0;
+        public readonly LevelHandle LevelHandle;
 
-        public Lesson(int compliteLevels, int levels)
+        public Lesson(int compliteLevels, int levels, LevelHandle levelHandle)
         {
             CompliteLevels = compliteLevels;
             Levels = levels;
+            LevelHandle = levelHandle;
         }
     }
 }
