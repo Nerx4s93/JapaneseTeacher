178acc5a75d5dc6c0bdf2c750d68be1820c98ba4
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-28 18:41:20 +0300

    Класс темы

---
+++ Lessons/Theme.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace JapaneseTeacher.Lessons
+{
+    internal class Theme
+    {
+        public readonly string Name;
+        public readonly string Description;
+        public readonly Color Color;
+        public readonly List<Lesson> Lessons;
+
+        public Theme(string name, string description, Color color, List<Lesson> lessons)
+        {
+            Name = name;
+            Description = description;
+            Color = color;
+            Lessons = lessons;
+        }
+    }
+}
