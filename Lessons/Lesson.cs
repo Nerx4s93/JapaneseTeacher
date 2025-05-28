d8162845d16f9f290d37df0c417240b32e4b9ab6
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-05-28 18:43:11 +0300

    Класс урока

---
+++ Lessons/Lesson.cs
@@ -0,0 +1,14 @@
+﻿namespace JapaneseTeacher.Lessons
+{
+    internal class Lesson
+    {
+        public readonly int CompliteLevels = 0;
+        public readonly int Levels = 0;
+
+        public Lesson(int compliteLevels, int levels)
+        {
+            CompliteLevels = compliteLevels;
+            Levels = levels;
+        }
+    }
+}
