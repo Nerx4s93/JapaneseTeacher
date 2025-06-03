2b962e66eff598b9a3eb3f585daf174e8e94ecfc
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-03 10:00:31 +0300

    Тестовый код: загрузка темы

--- GUI/FormMain.Designer.cs
+++ GUI/FormMain.Designer.cs
@@ -28,20 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.moduleHeader1 = new JapaneseTeacher.UI.ModuleHeader();
             this.SuspendLayout();
+            // 
+            // moduleHeader1
+            // 
+            this.moduleHeader1.BackColor = System.Drawing.Color.Transparent;
+            this.moduleHeader1.BackgroundColor = System.Drawing.Color.Orange;
+            this.moduleHeader1.Description = "Описание";
+            this.moduleHeader1.DescriptionColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.moduleHeader1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.moduleHeader1.Location = new System.Drawing.Point(61, 12);
+            this.moduleHeader1.Name = "moduleHeader1";
+            this.moduleHeader1.Size = new System.Drawing.Size(654, 107);
+            this.moduleHeader1.TabIndex = 0;
+            this.moduleHeader1.Tag = "1";
+            this.moduleHeader1.Text = "moduleHeader1";
+            this.moduleHeader1.Theme = "Тема";
+            this.moduleHeader1.ThemeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(179)))));
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.moduleHeader1);
+            this.CurrentSceneId = 1;
             this.DoubleBuffered = true;
             this.Name = "FormMain";
             this.Text = "Изучение японского";
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private UI.ModuleHeader moduleHeader1;
     }
 }
--- GUI/FormMain.cs
+++ GUI/FormMain.cs
@@ -1,16 +1,27 @@
-﻿using JapaneseTeacher.Data;
+﻿using System;
+
+using JapaneseTeacher.Data;
 using JapaneseTeacher.GUI.Presenters;
+using JapaneseTeacher.Tools;
 
 namespace JapaneseTeacher.GUI
 {
     internal partial class FormMain : SceneForm
     {
         private readonly GlobalData _globeData;
+        private readonly ThemeBuilder _themeBuilder;
 
         public FormMain(GlobalData globalData)
         {
             InitializeComponent();
             _globeData = globalData;
+            _themeBuilder = new ThemeBuilder(this);
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            var theme = _globeData.GetThemeByName("Hiragana");
+            _themeBuilder.Build(theme);
         }
     }
 }
