caa4049c3f04303155820ef7d0268220b0f76c41
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-03 11:38:33 +0300

    Доработка кода

--- Tools/ThemeBuilder.cs
+++ Tools/ThemeBuilder.cs
@@ -12,19 +12,14 @@ namespace JapaneseTeacher.Tools
     {
         private readonly Form _form;
 
-        private readonly ModuleHeader _moduleHeader = new ModuleHeader();
-        private readonly List<ButtonLevel> _buttonLevels = new List<ButtonLevel>();
-
         public ThemeBuilder(Form form)
         {
             _form = form;
-
-            _form.Controls.Add(_moduleHeader);
-            _moduleHeader.Size = new Size(700, 110);
-            _moduleHeader.Tag = 1;
-
             _form.Resize += Form_Resize;
         }
+
+        private ModuleHeader _moduleHeader = new ModuleHeader();
+        private List<ButtonLevel> _buttonLevels = new List<ButtonLevel>();
 
         private bool _update;
 
@@ -42,21 +37,27 @@ namespace JapaneseTeacher.Tools
 
         private void AdjustControls(Theme theme)
         {
-            _moduleHeader.Theme = theme.Name;
-            _moduleHeader.Description = theme.Description;
-
+            _moduleHeader.Dispose();
             foreach (var buttonLevel in _buttonLevels)
             {
                 buttonLevel.Dispose();
             }
+
+            _moduleHeader = new ModuleHeader();
+            _moduleHeader.Size = new Size(700, 110);
+            _moduleHeader.Tag = 1;
+            _moduleHeader.Theme = theme.Name;
+            _moduleHeader.Description = theme.Description;
 
             var levels = theme.GetLevels();
             foreach (var level in levels)
             {
                 var button = new ButtonLevel();
                 _buttonLevels.Add(button);
+                _form.Controls.Add(button);
             }
 
+            _form.Controls.Add(_moduleHeader);
             Form_Resize(null, null);
         }
 
