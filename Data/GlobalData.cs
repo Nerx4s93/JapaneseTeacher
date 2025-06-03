6d07b07a362712ca94e10d1e3e0aa234f877a552
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-03 10:00:00 +0300

    Метод, для получения темы из имени

--- Data/GlobalData.cs
+++ Data/GlobalData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace JapaneseTeacher.Data
 {
@@ -28,6 +29,11 @@ namespace JapaneseTeacher.Data
             }
 
             #endregion
+        }
+
+        public Theme GetThemeByName(string name)
+        {
+            return themes.First(t => t.Name == name);
         }
     }
 }
