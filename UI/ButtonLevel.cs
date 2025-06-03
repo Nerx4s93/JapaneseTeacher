0910c1bac9ce4c150206e8c638c6b6db41ef4303
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-03 16:39:26 +0300

    Отрисовка кнопки в высоком качестве

--- UI/ButtonLevel.cs
+++ UI/ButtonLevel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 using JapaneseTeacher.Tools;
@@ -81,6 +82,10 @@ namespace JapaneseTeacher.UI
         protected override void OnPaint(PaintEventArgs e)
         {
             var graphics = e.Graphics;
+
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
 
             int x = PercentToPixels(3);
             if (Active)
