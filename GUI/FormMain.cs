a270a4de12cf58cab790ac0927f7d6caee2bf3c7
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-03 17:07:53 +0300

    Тестовый код для загрузки уровня

--- GUI/FormMain.cs
+++ GUI/FormMain.cs
@@ -2,6 +2,7 @@
 
 using JapaneseTeacher.Components;
 using JapaneseTeacher.Data;
+using JapaneseTeacher.GUI.Components;
 using JapaneseTeacher.GUI.Presenters;
 
 namespace JapaneseTeacher.GUI
@@ -9,19 +10,22 @@ namespace JapaneseTeacher.GUI
     internal partial class FormMain : SceneForm
     {
         private readonly GlobalData _globeData;
+
         private readonly ThemeBuilder _themeBuilder;
+        private readonly LevelBuilder _levelBuilder;
 
         public FormMain(GlobalData globalData)
         {
             InitializeComponent();
             _globeData = globalData;
             _themeBuilder = new ThemeBuilder(this);
+            _levelBuilder = new LevelBuilder(this);
         }
 
         private void FormMain_Shown(object sender, EventArgs e)
         {
             var theme = _globeData.GetThemeByName("Hiragana");
-            _themeBuilder.Build(theme);
+            _levelBuilder.LoadLevel(theme, "1");
         }
     }
 }
