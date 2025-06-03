87fedf54cc1b711a269150150d876669d0ef3397
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-03 10:00:16 +0300

    Класс, для настройки показа уровня

---
+++ Tools/ThemeBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Forms;
+
+using JapaneseTeacher.Data;
+using JapaneseTeacher.UI;
+
+namespace JapaneseTeacher.Tools
+{
+    internal class ThemeBuilder
+    {
+        private readonly Form _form;
+
+        private readonly ModuleHeader _moduleHeader = new ModuleHeader();
+
+        public ThemeBuilder(Form form)
+        {
+            _form = form;
+        }
+
+        private bool _update;
+
+        public void Build(Theme theme)
+        {
+            _update = true;
+            AdjustControls(theme);
+
+        }
+
+        private void AdjustControls(Theme theme)
+        {
+
+        }
+
+        public void StopHandle()
+        {
+            _update = false;
+        }
+    }
+}
