7defd6ede36160281bdd50d2a43257eeae948d64
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-03 17:06:32 +0300

    Класс для создания сцены уровня

---
+++ GUI/Components/LevelBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+using JapaneseTeacher.Data;
+
+namespace JapaneseTeacher.GUI.Components
+{
+    internal class LevelBuilder
+    {
+        private readonly Form _form;
+
+        public LevelBuilder(Form form)
+        {
+            _form = form;
+        }
+
+        private Theme _theme;
+        private string _levelId;
+
+        public void LoadLevel(Theme theme, string levelId)
+        {
+            _theme = theme;
+            _levelId = levelId;
+        }
+    }
+}
