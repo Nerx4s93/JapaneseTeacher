888cea78d1826966ad7060e0788cb2b2bba5fa1a
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-03 10:09:27 +0300

    Настройка заголовка уровня

--- Tools/ThemeBuilder.cs
+++ Tools/ThemeBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 using JapaneseTeacher.Data;
 using JapaneseTeacher.UI;
@@ -14,6 +16,11 @@ namespace JapaneseTeacher.Tools
         public ThemeBuilder(Form form)
         {
             _form = form;
+
+            _form.Controls.Add(_moduleHeader);
+            _moduleHeader.Tag = 1;
+
+            _form.Resize += Form_Resize;
         }
 
         private bool _update;
@@ -25,14 +32,24 @@ namespace JapaneseTeacher.Tools
 
         }
 
-        private void AdjustControls(Theme theme)
-        {
-
-        }
-
         public void StopHandle()
         {
             _update = false;
+        }
+
+        private void AdjustControls(Theme theme)
+        {
+            _moduleHeader.Theme = theme.Name;
+            _moduleHeader.Description = theme.Description;
+            _moduleHeader.Size = new Size(700, 110);
+
+            Form_Resize(null, null);
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            var x = (_form.Size.Width - _moduleHeader.Size.Width) / 2;
+            _moduleHeader.Location = new Point(x, 10);
         }
     }
 }
