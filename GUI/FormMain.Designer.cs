a4564b2e6985944b9f19d25487dd8bacdb625b11
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-04 18:20:35 +0300

    Исправление ошибок

--- GUI/FormMain.Designer.cs
+++ GUI/FormMain.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.CurrentSceneId = 2;
+            this.Name = "FormMain";
             this.Text = "FormMain";
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
