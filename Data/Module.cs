080cfcd19387d12cf37270d3c3c925c1e815dd98
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-07 17:27:53 +0300

    Объект модуля, содержит темы для изучения

---
+++ Data/Module.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace JapaneseTeacher.Data
+{
+    internal class Module
+    {
+        public string Name { get; set; }
+        public List<Theme> Themes { get; set; }
+    }
+}
