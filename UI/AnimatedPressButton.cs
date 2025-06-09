9de3ab33240bbf978b35ff16cbcb215233fd4237
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-09 18:16:27 +0300

    Сортировка using

--- UI/AnimatedPressButton.cs
+++ UI/AnimatedPressButton.cs
@@ -1,9 +1,9 @@
-﻿using JapaneseTeacher.Tools;
-using System;
+﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.IO;
 using System.Windows.Forms;
+
+using JapaneseTeacher.Tools;
 
 namespace JapaneseTeacher.UI
 {
