6f65ab3ad5c4a46b974f2e29282d504d3fac2373
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-06-10 16:32:40 +0300

    Типы уровней

--- Data/Level.cs
+++ Data/Level.cs
@@ -1,9 +1,12 @@
-﻿namespace JapaneseTeacher.Data
+﻿using System.Collections.Generic;
+
+namespace JapaneseTeacher.Data
 {
     internal class Level
     {
         public string LevelId;
         public string Description;
+        public List<LevelType> LevelTypes;
         public int CompletedSublevels;
         public int TotalSublevels;
     }
---
+++ Data/LevelType.cs
@@ -0,0 +1,9 @@
+﻿namespace JapaneseTeacher.Data
+{
+    internal enum LevelType
+    {
+        JapaneseToReading = 0,
+        TranslateToReading = 1,
+        ReadingToTranslate = 2
+    }
+}
